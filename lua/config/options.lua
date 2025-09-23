e1269f8b541455c87efa244182490aff2e867d3a
aisng <tasignas@gmail.com>
2025-09-23 21:03:19 +0300

    override hover with border

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -20,3 +20,13 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.undofile = true
+
+local hover = vim.lsp.buf.hover
+-- @diagnostic disable-next-line: duplicate-set-field
+vim.lsp.buf.hover = function()
+	return hover({
+		max_width = 100,
+		max_height = 20,
+		border = "rounded",
+	})
+end
