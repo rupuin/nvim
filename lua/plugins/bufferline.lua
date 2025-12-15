d279e706cfcfa0fb5d9f73f2697e70584ecb9523
rupuin <tasignas@gmail.com>
2025-12-16 00:09:00 +0100

    show help buffers in bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -9,15 +9,18 @@ return {
 			separator_style = "thin",
 			diagnostics = "nvim_lsp",
 			max_name_length = 30,
-			numbers = "buffer_id",
 		},
 	},
 	config = function(_, opts)
 		require("bufferline").setup(opts)
-
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "help",
+			callback = function()
+				vim.bo.buflisted = true
+			end,
+		})
 		local keymap = vim.keymap.set
 
-		-- navigate visually in bufferline
 		keymap("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Prev buffer" })
 		keymap("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
 	end,
