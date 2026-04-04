20d2469bf12086d960495900e81dd29758ebe796
rupuin <tasignas@gmail.com>
2026-04-04 19:47:17 +0200

    treesiter updates for nvim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,46 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
 	branch = "main",
+	build = ":TSUpdate",
+	lazy = false,
 	config = function()
-		require("nvim-treesitter").setup({
-			ensure_installed = {
-				"css",
-				"html",
-				"tsx",
-				"lua",
-				"bash",
-				"ruby",
-				"typescript",
-				"solidity",
-				"markdown",
-				"python",
-				"properties",
-				"dart",
-				"c",
-			},
-			sync_install = true,
-			auto_install = false,
-			highlight = {
-				enable = true,
-				disable = function(_, buf)
-					local max_filesize = 50 * 1024
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					return ok and stats and stats.size > max_filesize
-				end,
-			},
-			indent = { enable = true },
+		local langs = {
+			go = { ft = { "go" } },
+			lua = { ft = { "lua" } },
+			bash = { ft = { "sh" } },
+			ruby = { ft = { "ruby" } },
+			python = { ft = { "python" } },
+			markdown = { ft = { "markdown" } },
+			markdown_inline = {},
+			html = { ft = { "html" } },
+			css = { ft = { "css" } },
+			typescript = { ft = { "typescript" } },
+			tsx = { ft = { "typescriptreact" } },
+			dart = { ft = { "dart" } },
+			c = { ft = { "c" } },
+			properties = { ft = { "properties" } },
+			solidity = { ft = { "solidity" } },
+		}
+
+		local parsers = {}
+		local patterns = {}
+
+		for parser, spec in pairs(langs) do
+			table.insert(parsers, parser)
+
+			for _, ft in ipairs(spec.ft or {}) do
+				table.insert(patterns, ft)
+			end
+		end
+
+		require("nvim-treesitter").setup()
+		require("nvim-treesitter").install(parsers)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = patterns,
+			callback = function(args)
+				pcall(vim.treesitter.start, args.buf)
+			end,
 		})
 	end,
 }
