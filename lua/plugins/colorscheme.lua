fd96df79bb4a3710698aac455323fff7fea06b38
rupuin <tasignas@gmail.com>
2026-04-06 01:44:05 +0200

    auto dark/light theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,36 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			auto_integrations = true,
-			integrations = {
-				fzf = true,
-			},
-		})
-
-		vim.cmd.colorscheme("catppuccin-macchiato")
-	end,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				auto_integrations = true,
+				integrations = {
+					fzf = true,
+				},
+			})
+		end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"f-person/auto-dark-mode.nvim",
+		opts = {
+			set_dark_mode = function()
+				vim.o.background = "dark"
+				vim.cmd.colorscheme("catppuccin-macchiato")
+			end,
+			set_light_mode = function()
+				vim.g.gruvbox_material_background = "hard"
+				vim.g.gruvbox_material_foreground = "material"
+				vim.o.background = "light"
+				vim.cmd.colorscheme("gruvbox-material")
+			end,
+		},
+	},
 }
